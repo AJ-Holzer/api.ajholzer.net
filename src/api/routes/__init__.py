d01feb808ea6d81effa2fbd073715c6c49ab9bff
AJ-Holzer <axel123.holzer@gmail.com>
2026-01-13 20:25:16 +0100

    Update `__init__.py` files.

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -4,16 +4,10 @@ from . import github
 from .github import list_repositories
 from . import health
 from .health import get_health
-from . import website
-from .website import run_update
-from .website import update_site
 
 __all__ = [
     "github",
     "health",
-    "website",
     "get_health",
     "list_repositories",
-    "run_update",
-    "update_site",
 ]
