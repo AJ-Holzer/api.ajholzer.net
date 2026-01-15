5af8c600febdb72899cff0a3c5ada91ab9b2af74
AJ-Holzer <axel123.holzer@gmail.com>
2026-01-15 14:00:50 +0100

    Remove bootstrap logging

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@
 
 import logging
 
-from utils.logging import setup_logging, setup_bootstrap_logging
+from utils.logging import setup_logging
 from api.app import API
 
 
@@ -14,14 +14,10 @@ logger: logging.Logger = logging.getLogger(name=__name__)
 
 
 def main() -> None:
-    # Set up bootstrap logging
-    setup_bootstrap_logging()
-
     # Load config
     from config import config
 
     # Set up logging from config
-    logger.debug("Setting up logging...")
     setup_logging(loglevel=config.LOGLEVEL, log_filepath=config.LOG_FILEPATH)
 
     # Check config
--- src/utils/logging.py
+++ src/utils/logging.py
@@ -51,11 +51,3 @@ def setup_logging(loglevel: int | str, log_filepath: Path) -> None:
             },
         }
     )
-
-
-def setup_bootstrap_logging() -> None:
-    """Set up bootstrap logging configuration."""
-    setup_logging(
-        loglevel=logging.DEBUG,
-        log_filepath=Path("api_bootstrap.log").resolve(),
-    )
